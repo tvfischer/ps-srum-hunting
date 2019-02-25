93185e096691343e571383184c62d77a08807470
Thomas V. Fischer <tvfischer@gmail.com>
2019-02-25 18:28:47 +0000

    Added WiP information, used to mark the thought process and testing the access

---
+++ WiP/OpeningESEFiles.ps1
@@ -0,0 +1,81 @@
+﻿			[System.Int32]$FileType = -1
+			[Microsoft.Isam.Esent.Interop.Api]::JetGetDatabaseFileInfo($Path, [ref]$FileType, [Microsoft.Isam.Esent.Interop.JET_DbInfo]::FileType)
+			[Microsoft.Isam.Esent.Interop.JET_filetype]$DBType = [Microsoft.Isam.Esent.Interop.JET_filetype]($FileType)
+
+			Write-Verbose -Message "File type $DBType."
+		
+			if ($DBType -eq [Microsoft.Isam.Esent.Interop.JET_filetype]::Database) 
+			{
+				if ($PageSize -eq -1 -or ($PageSize % 1024 -ne 0)) 
+				{
+					[Microsoft.Isam.Esent.Interop.Api]::JetGetDatabaseFileInfo($Path, [ref]$PageSize, [Microsoft.Isam.Esent.Interop.JET_DbInfo]::PageSize)
+				}
+
+
+				[Microsoft.Isam.Esent.Interop.JET_INSTANCE]$Instance = New-Object -TypeName Microsoft.Isam.Esent.Interop.JET_INSTANCE
+				[Microsoft.Isam.Esent.Interop.JET_SESID]$Session = New-Object -TypeName Microsoft.Isam.Esent.Interop.JET_SESID
+
+				$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetSetSystemParameter($Instance, [Microsoft.Isam.Esent.Interop.JET_SESID]::Nil, [Microsoft.Isam.Esent.Interop.JET_param]::DatabasePageSize, $PageSize, $null)
+				$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetSetSystemParameter($Instance, [Microsoft.Isam.Esent.Interop.JET_SESID]::Nil, [Microsoft.Isam.Esent.Interop.JET_param]::Recovery, [int]$Recovery, $null)
+				$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetSetSystemParameter($Instance, [Microsoft.Isam.Esent.Interop.JET_SESID]::Nil, [Microsoft.Isam.Esent.Interop.JET_param]::CircularLog, [int]$CircularLogging, $null)
+
+				[Microsoft.Isam.Esent.Interop.Api]::JetCreateInstance2([ref]$Instance, "Instance", "Instance", [Microsoft.Isam.Esent.Interop.CreateInstanceGrbit]::None)
+				$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetInit2([ref]$Instance, [Microsoft.Isam.Esent.Interop.InitGrbit]::None)
+				[Microsoft.Isam.Esent.Interop.Api]::JetBeginSession($Instance, [ref]$Session, $UserName, $Password)
+
+				[Microsoft.Isam.Esent.Interop.JET_DBID]$DatabaseId = New-Object -TypeName Microsoft.Isam.Esent.Interop.JET_DBID
+
+				try 
+				{
+					try 
+					{
+						$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetAttachDatabase($Session, $Path, [Microsoft.Isam.Esent.Interop.AttachDatabaseGrbit]::ReadOnly)
+						$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetOpenDatabase($Session, $Path, $Connect, [ref]$DatabaseId, [Microsoft.Isam.Esent.Interop.OpenDatabaseGrbit]::ReadOnly)
+					}
+					catch [Exception] {
+
+						Write-Verbose -Message $_.Exception.Message
+						Write-Verbose -Message "Running recovery on $Path with log prefix $LogPrefix."
+
+						& "$env:SystemRoot\System32\esentutl.exe" "/r" "$LogPrefix"
+
+						try 
+						{
+							$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetAttachDatabase($Session, $Path, [Microsoft.Isam.Esent.Interop.AttachDatabaseGrbit]::ReadOnly)
+							$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetOpenDatabase($Session, $Path, $Connect, [ref]$DatabaseId, [Microsoft.Isam.Esent.Interop.OpenDatabaseGrbit]::ReadOnly)
+						}
+						catch [Exception] 
+						{
+							Write-Verbose -Message "Recovery failed, running repair on $Path."
+							& "$env:SystemRoot\System32\esentutl.exe" "/p" "$Path" "/o"
+							$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetAttachDatabase($Session, $Path, [Microsoft.Isam.Esent.Interop.AttachDatabaseGrbit]::ReadOnly)
+							$Temp = [Microsoft.Isam.Esent.Interop.Api]::JetOpenDatabase($Session, $Path, $Connect, [ref]$DatabaseId, [Microsoft.Isam.Esent.Interop.OpenDatabaseGrbit]::ReadOnly)
+						}
+					}
+				}
+				catch [Exception] 
+				{
+					Write-Verbose -Message $_.Exception.Message
+					Write-Verbose -Message "Shutting down database due to exception."
+
+					try 
+					{
+						[Microsoft.Isam.Esent.Interop.Api]::JetDetachDatabase($Session, $Path)					
+					}
+					finally
+					{
+						[Microsoft.Isam.Esent.Interop.Api]::JetEndSession($Session, [Microsoft.Isam.Esent.Interop.EndSessionGrbit]::None)
+						[Microsoft.Isam.Esent.Interop.Api]::JetTerm($Instance)
+						Write-Verbose -Message "Completed shut down successfully."
+						throw $_.Exception
+					}
+				}
+			}
+			else 
+			{
+				throw "The path must be to a database, the selected path was a $DBType."
+			}
+
+			Write-Output -InputObject ([PSCustomObject]@{Instance=$Instance;Session=$Session;DatabaseId=$DatabaseId;Path=$Path})
+		}
+	}
